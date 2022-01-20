4a8783ebd6d6743ec42d65238b5c4fb3c4f61f88
Salman Abbas <salmanabbas985@gmail.com>
2022-01-20 20:58:01 +0300

    BelogsTo author

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the author that owns the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
 }
