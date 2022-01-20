7ea330fad30f42f7f09b160911e68f8edf1ac0a3
Salman Abbas <salmanabbas985@gmail.com>
2022-01-20 20:59:09 +0300

    routes updated

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PostController::class, 'index']);
+Route::post('/posts', [PostController::class, 'search'])->name('search');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
