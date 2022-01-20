a2630922b3de0b7330b238199fb2a212998346ea
Salman Abbas <salmanabbas985@gmail.com>
2022-01-20 20:57:44 +0300

    Search posts by author or title

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,14 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::paginate(8);
         
         return view('posts.index')->with(['posts'=> $posts]);
     }
+
+    public function search(Request $request)
+    {
+        $user = User::where('name','like','%'.$request->search.'%')->first();
+
+
+        if ($user == null) {
+            $posts = Post::where('title','like','%'.$request->search.'%')->get();
+        }
+        else{
+            $posts = Post::where('title','like','%'.$request->search.'%')
+            ->orWhere('user_id','like','%'.$user->id.'%')->get();
+        }
+
+        return view('posts.index')->with(['posts'=> $posts]);
+
+    }
+
 }
