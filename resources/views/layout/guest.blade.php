ffcda36938d73f10c3dcee7fc1074f7e471e0632
Salman Abbas <salmanabbas985@gmail.com>
2022-01-22 15:58:55 +0300

    updated

--- resources/views/layout/guest.blade.php
+++ resources/views/layout/guest.blade.php
@@ -7,10 +7,10 @@
     <title>Blog @yield('title')</title>
 
     <!-- Styles -->
-    <link rel="stylesheet" href="/css/app.css'">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     <!-- Scripts -->
-    <script src="/js/app.js" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 <body>
     
